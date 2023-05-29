3a8a9a58330b370ccbba40cec8b47c3cb23f730a
Tuan Anh Nguyen <21anh.nt@vinuni.edu.vn>
2023-05-29 15:29:13 +0700

    Update test file

---
+++ test_hashtable.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcc -o hashtable_main hashtable_main.c hashtable.c linkedlist.c && ./hashtable_main
---
+++ test_linkedlist.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -o linkedlist_main linkedlist_main.c linkedlist.c && ./linkedlist_main
